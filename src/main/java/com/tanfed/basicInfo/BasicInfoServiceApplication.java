37dceb78e93b6849410db14876065323dc271898
TANFEDIT <tanfeditteam@gmail.com>
2025-09-03 18:39:39 +0530

    test api

--- src/main/java/com/tanfed/basicInfo/BasicInfoServiceApplication.java
+++ src/main/java/com/tanfed/basicInfo/BasicInfoServiceApplication.java
@@ -1,15 +1,22 @@
 package com.tanfed.basicInfo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class BasicInfoServiceApplication {
 
+	@GetMapping("/test")
+	public String test() throws Exception {
+		return "Service Deployed Successfully!";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BasicInfoServiceApplication.class, args);
 	}
