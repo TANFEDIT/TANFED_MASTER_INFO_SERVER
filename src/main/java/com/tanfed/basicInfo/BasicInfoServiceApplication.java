17bb331bc6d6146673a6d88ad1ed21decfcda0e9
TANFEDIT <tanfeditteam@gmail.com>
2025-09-03 19:41:44 +0530

    test api

--- src/main/java/com/tanfed/basicInfo/BasicInfoServiceApplication.java
+++ src/main/java/com/tanfed/basicInfo/BasicInfoServiceApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@RestController
 public class BasicInfoServiceApplication {
 
 	@GetMapping("/test")
